704e91eb0235b2833e1a6aebc1a6f0a320f550b7
bhumistwt <bhumimishra99600@gmail.com>
2025-01-22 22:05:40 +0530

    hero sec view button

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,17 +3,57 @@ import { FC } from "react";
 import heroImage from "@/assets/images/hero-image.jpg";
 import Image from "next/image";
 import Button from "@/components/Button";
+import SplitType from "split-type";
+import { useAnimate,  motion, stagger } from "motion/react";
+import { useEffect } from "react";
+import { transform } from "next/dist/build/swc";
 
 const Hero: FC = () => {
+
+  const[titleScope, titleAnimate] = useAnimate();
+
+  useEffect(() => {
+      new SplitType(titleScope.current, {
+        types: "lines,words",
+        tagName: "span",
+      });
+
+      titleAnimate(titleScope.current.querySelectorAll('word'), {
+        transform: "translateY(0)"
+      }, {
+          duration: 0.5,
+          delay: stagger(0.2),
+      }
+    );
+    
+    },
+  []);
+
+
+
+
   return (
     <section>
       <div className="grid md:grid-col-12 md:h-screen items-strecth">
         <div className="md:col-span-7 flex-col justify-center">
           <div className="container !max-w-full">
-            <h1 className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0">
+            <motion.h1 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0"
+            ref={titleScope}
+            >
               Crafting digital experiences through code and creative design
-            </h1>
+            </motion.h1>
             <div className="flex flex-col md:flex-row md:items-center  mt-10 items-start gap-6">
+              <motion.div 
+              initial={{ opacity: 0, y: "100%" }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.5,
+                delay: 1.75,
+              }}
+              >
               <Button
                 variant="secondary"
                 iconAfter={
@@ -35,7 +75,17 @@ const Hero: FC = () => {
               >
                 <span>View My Work</span>
               </Button>
+              </motion.div>
+              <motion.div
+              initial={{ opacity: 0, y: "100%" }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.5,
+                delay: 2.2,
+              }}
+              >
               <Button variant="text">Let&apos;'s Talk</Button>
+              </motion.div>
             </div>
           </div>
         </div>
