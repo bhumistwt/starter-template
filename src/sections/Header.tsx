aa95ee5f9a034e6a8ae720c56af98e75aaddc2c1
bhumistwt <bhumimishra99600@gmail.com>
2025-01-22 09:37:43 +0530

    headerani

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,7 @@
-import { FC } from "react";
+"use client";
+import { FC, useEffect, useState } from "react";
 import Button from "@/components/Button";
+import { motion, useAnimate } from 'motion/react';
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const navItems = [
@@ -26,19 +28,106 @@ const navItems = [
 ];
 
 const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [topLineScope, topLineAnimate] = useAnimate();
+  const [bottomLineScope, bottomLineAnimate] = useAnimate();
+ 
+  useEffect(() => {
+    if (isOpen) {
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            translateY: 4,
+          },
+        ],
+        [
+          topLineScope.current,
+          {
+            rotate: 45,
+          },
+        ],
+      ]);
+
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            translateY: -4,
+          },
+        ],
+        [
+          bottomLineScope.current,
+          {
+            rotate: -45,
+          },
+        ],
+      ]);
+    } else{
+      topLineAnimate([
+        [
+      topLineScope.current,
+      {
+        rotate: 0
+      },
+    ],
+    [
+      topLineScope.current,
+      {
+        translateY: 0
+      },
+    ],
+    ]);
+
+    bottomLineAnimate([
+      [
+    bottomLineScope.current,
+    {
+      rotate: 0
+    },
+  ],
+  [
+    bottomLineScope.current,
+    {
+      translateY: 0
+    },
+  ],
+  ]);
+    }
+  }, [isOpen, topLineAnimate, topLineScope, bottomLineAnimate, bottomLineScope]);
+
   return (
-    <header className="fixed top-0 left-0 w-full backdrop-blur-md">
+    <header>
+      <div className="fixed top-0 left-0 w-full bg-stone-900">
+      <nav className="mt-20 flex flex-col">
+      {navItems.map(({ label, href }) => (
+        <a href={href} key={label}
+        className="text-stone-200"
+        >
+        <span>{label}</span>
+        </a> 
+          ))}
+      </nav>
+      </div>
+      <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md"></div>
       <div className="container !max-w-full">
         <div className="flex justify-between h-20 items-center">
           <div>
             <a href="/">
-              <span className="text-xl font-bold uppercase">
+              <span className="text-xl font-bold uppercase text-white">
                 Alex&nbsp; Taylor
               </span>
             </a>
           </div>
+          </div>
+          </div>
+      <div className="fixed top-0 left-0 w-full ">   
+       <div className="container !max-w-full">
+        <div className="flex justify-end h-20 items-center">
           <div className="flex items-center gap-4">
-            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
+            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200" 
+            onClick={() => setIsOpen(!isOpen)}
+            >
               <svg
                 width="24"
                 height="24"
@@ -46,8 +135,28 @@ const Header: FC = () => {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <rect x="3" y="15" width="18" height="2" fill="currentColor" />
-                <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+                 <motion.rect 
+                x="3" y="7" 
+                width="18" 
+                height="2" 
+                fill="currentColor"
+                ref={topLineScope}
+                style={{
+                  transformOrigin: "12px 8px",
+                }} 
+                />
+                <motion.rect
+                x="3" 
+                y="15" 
+                width="18" 
+                height="2" 
+                fill="currentColor"
+                ref={bottomLineScope}
+                style={{
+                  transformOrigin: "12px 16px",
+                }}
+                />
+              
               </svg>
             </div>
             <Button variant="primary" className=" hidden md:inline-flex">
@@ -56,6 +165,8 @@ const Header: FC = () => {
           </div>
         </div>
       </div>
+      </div>
+     
     </header>
   );
 };
